aa2a3b3f277a0d0920cd2a3725a894d1188814c6
erickaeuro <capistranoerickaeuro@gmail.com>
2022-10-06 00:20:02 +0800

    try

--- pages/inventory/stocks.php
+++ pages/inventory/stocks.php
@@ -152,7 +152,7 @@
                         $move = "Update inventorytbl set move = '1' where stock_no = '$stock'";
                         $insert_row = $con->query($move) or die ($con->error.__LINE__);
                         echo '<script>alert("Moved")</script>';
-                        //eader("Location:stocks.php");
+                        
                     }
               
                 }
