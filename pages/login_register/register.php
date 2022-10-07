423ab50b620b7e36d6f621da14c9c8542e5d4e21
edslr8 <102961702+edslr8@users.noreply.github.com>
2022-10-08 00:24:03 +0800

    kahitano

--- pages/login_register/register.php
+++ pages/login_register/register.php
@@ -28,6 +28,18 @@
   	  <label>Confirm password</label>
   	  <input type="password" name="password_2">
   	</div>
+	
+	  <form name="security" id="security" action="">
+	  Security Questions: <select name="security_qstn">
+    <option value="" selected="selected">Please select security questions</option>
+	<option >What is your name of your favorite pet?</option>
+	<option >What was your favorite food as a child?</option>
+	<option >What was the first thing you learned to cook?</option>
+  </select>
+  <div class="input-group">
+  <input type="text" name="security_ans">
+</div>
+	  
   	<div class="input-group">
   	  <button type="submit" class="btn" name="reg_user">Register</button>
   	</div>
