f506bfd0724c6d8c21143b8034ce577ad48844c1
erickaeuro <capistranoerickaeuro@gmail.com>
2022-10-08 21:22:08 +0800

    stocks

--- pages/inventory/stockadd.php
+++ pages/inventory/stockadd.php
@@ -86,7 +86,7 @@
 
                             <div class="form-group col-md-12">
                                 <label for="tagprice"><b>Tag Price</b></label>
-                                <input type="text" class="form-control" name="tagprice" placeholder="P 0.00">
+                                <input type="text" class="form-control" name="tagprice" placeholder="P 0.00" required>
                             </div>
 
                             <div class="form-group col-md-12">
