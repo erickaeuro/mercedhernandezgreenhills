7bedc049de2f1719856a5a82249e07ff18df0093
erickaeuro <capistranoerickaeuro@gmail.com>
2022-10-08 21:14:55 +0800

    tryyyyy

--- pages/connection.php
+++ pages/connection.php
@@ -7,15 +7,3 @@
 $connect = new PDO('mysql:host=localhost;dbname=mercedhernandezgreenhills','root', '');
 ?> 
 
-
-<?php 
-
- // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
-  if (empty($item_type)) { array_push($errors, "Item Type is required"); }
-  if (empty($karat_gold)) { array_push($errors, "Karat/Gold is required"); }
-  if (empty($kindofstone)) { array_push($errors, "Kind of Stone is required"); }
-  if (empty($tagprice)) { array_push($errors, "Tag Price is required"); }
-  
-
-?> 
