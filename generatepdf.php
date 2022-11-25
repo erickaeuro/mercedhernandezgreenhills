201ceb9c72e24c07caced10ee2d52a07d03364ce
MrScout11 <tetsuchi10@gmail.com>
2022-11-25 09:09:35 +0800

    New design pdf

--- generatepdf.php
+++ generatepdf.php
@@ -174,32 +174,55 @@ $pdf->Cell(20, 5, '(Signature or Thumbmark) Pawner ',0,0);
 $pdf->Cell(118,1,'',0,0);
 $pdf->Cell(55, 5, 'Pawnshops Authorized Representative', 0, 0);
 
+$pdf->SetFont('times', '', 10);
 $pdf->AddPage();
-
-$pdf->MultiCell(200, 2,'TERMS AND CONDITIONS
-1.  The pawner hereby accepts the pawnshops appraisal as proper.
-2.  The interest rate stipulated herein is in accordance with the existing policy of the Monetary Board. The pawnshop hereby agrees not to collect in advance interest for a period of more than one (1) year. For purposes of computing the amount of interest for pledge loans paid after the maturity date, a fraction of (less than) a month shall be considered as one whole month.
-3.  The service charge is equivalent to one percent (1%) of the principal loan, but not to exceed five pesos (P5.00). No other charges shall be collected.
-4.  This loan is renewable for such amount and period as may be agreed upon between the pawnshop and the pawner, subject to the requirements of P.D. 114 for a new loan.
+$left_column = '<b>TERMS AND CONDITION</b><br>
+1.  The pawner hereby accepts the pawnshops appraisal as proper.<br>
+2.  The interest rate stipulated herein is in accordance with the existing policy of the Monetary Board. The pawnshop hereby agrees not to collect in advance interest for a period of more than one (1) year. For purposes of computing the amount of interest for pledge loans paid after the maturity date, a fraction of (less than) a month shall be considered as one whole month.<br>
+3.  The service charge is equivalent to one percent (1%) of the principal loan, but not to exceed five pesos (P5.00). No other charges shall be collected.<br>
+4.  This loan is renewable for such amount and period as may be agreed upon between the pawnshop and the pawner, subject to the requirements of P.D. 114 for a new loan.<br>
 5.  Upon maturity of this loan, as indicated on the face of this ticket, the pawner still has ninety (90) days from maturity date within which to redeem the pawn by paying the principal loan plus the interest that shall have accrued thereon. The amount of interest due and payable after the maturity date of the loan and during the redemption period shall be computed upon redemption at the same rate of interest provided in No.2 above based on the sum of the principal loan and interest earned as of the date of maturity.
-In case of pre-payment of this loan by pawner, the interest collected in advance shall accrue in full to the pawnshop.
+In case of pre-payment of this loan by pawner, the interest collected in advance shall accrue in full to the pawnshop.<br>
 6.  The pawner and the pawnshop agree that the notice of auction sale shall be delivered via ( ) e-mail or ( ) SMS. In case no mode of notification is agreed upon, the default shall be via ordinary mail. The pawnshop shall have the right to sell or dispose of the pawn in public auction if the pawner fails to redeem the pawn within ninety (90) days grace period.
-The pawnshop shall send the reminder on or before the expiration of ninety (90) day grace period.
-7.  The parties hereby agree that this ticket shall be surrendered at maturity date upon payment of the loan. In case of loss or destruction of this ticket, the pawner hereby undertakes to personally present an affidavit to the pawnshop before the redemption period expires. It is hereby agreed that the pawnshop has a period of two (2) days within which to verify from its record before ( ) indicating of the affidavit that it shall take the place of the original pawn ticket for purposes of redemption or ( ) issuing a substitute ticket, the original pawn ticket being deemed cancelled.
-8.  The pawner hereby agrees not to assign, sell or in any other way alienate the pawn securing this loan as evidenced by the pawn ticket without prior written consent of the pawnshop subject to the terms and conditions of this contract.
-9.  The pawner shall not be entitled to the excess of the public auction sale price over the amount of principal, interest and service fee; neither shall the pawnshop be entitled to recover the deficiency from the pawner.
-10. In case this loan is not paid on maturity date, the pawner hereby agrees to pay in addition to accrued interest, 2% per month of the principal, as liquidated damages. For purpose of computing the amount of liquidated damages, a fraction of a month shall be considered as one (1) full month. The pawnshop may, at its sole option, allow redemption of pawn after expiration of the 90-day grace period upon payment by the pawner of the loan principal plus interest due and liquidated damage at the rates and manner of computation herein prescribed.
-11. The pawnshop shall not be liable for loss or damage of the pawn due to fortuitous events without fault or negligence on its part, while the pawn is in its possession.
-12. The pawnshop shall exercise reasonable care and caution that an ordinary prudent person would as to his own property over the thing pawned in accordance with Republic Act No. 386 (Civil Code of the Philippines), as amended. Accordingly, the pawnshop shall insure all pawned items, except those which are kept inside a fireproof vault, in accordance with the pertinent regulation of the Bangko Sentral ng Pilipinas. Claims for retitution by pawner in case of loss, destruction or defect of the pawn due to robbery, fire and other directors and officers are cognizable by the regular courts.
-13. Venue of all judicial and administrative cases or proceedings and other legal incidents arising out of or in connection with this contract shall solely and exclusively be brought before the appropriate courts, departments, offices or agencies of the government situated in Mandaluyong City.
-14. The authorized representative must present valid identification papers.
-15. The pawner shall advise the pawnshop of any change of address.
-16. Acknowledgment: I hereby declare that the above mentioned article(s) is/are my personal property and are free from liens and encumbrances.
+The pawnshop shall send the reminder on or before the expiration of ninety (90) day grace period.<br>
+7.  The parties hereby agree that this ticket shall be surrendered at maturity date upon payment of the loan. In case of loss or destruction of this ticket, the pawner hereby undertakes to personally present an affidavit to the pawnshop before the redemption period expires. It is hereby agreed that the pawnshop has a period of two (2) days within which to verify from its record before ( ) indicating of the affidavit that it shall take the place of the original pawn ticket for purposes of redemption or ( ) issuing a substitute ticket, the original pawn ticket being deemed cancelled.<br>
+8.  The pawner hereby agrees not to assign, sell or in any other way alienate the pawn securing this loan as evidenced by the pawn ticket without prior written consent of the pawnshop subject to the terms and conditions of this contract.';
 
-_____________________
-PAWNER
+$right_column = '9. The pawner shall not be entitled to the excess of the public auction sale price over the amount of principal, interest and service fee; neither shall the pawnshop be entitled to recover the deficiency from the pawner.<br><br>
+10. In case this loan is not paid on maturity date, the pawner hereby agrees to pay in addition to accrued interest, 2% per month of the principal, as liquidated damages. For purpose of computing the amount of liquidated damages, a fraction of a month shall be considered as one (1) full month. The pawnshop may, at its sole option, allow redemption of pawn after expiration of the 90-day grace period upon payment by the pawner of the loan principal plus interest due and liquidated damage at the rates and manner of computation herein prescribed.<br><br>
+11. The pawnshop shall not be liable for loss or damage of the pawn due to fortuitous events without fault or negligence on its part, while the pawn is in its possession.<br><br>
+12. The pawnshop shall exercise reasonable care and caution that an ordinary prudent person would as to his own property over the thing pawned in accordance with Republic Act No. 386 (Civil Code of the Philippines), as amended. Accordingly, the pawnshop shall insure all pawned items, except those which are kept inside a fireproof vault, in accordance with the pertinent regulation of the Bangko Sentral ng Pilipinas. Claims for retitution by pawner in case of loss, destruction or defect of the pawn due to robbery, fire and other directors and officers are cognizable by the regular courts.<br><br>
+13. Venue of all judicial and administrative cases or proceedings and other legal incidents arising out of or in connection with this contract shall solely and exclusively be brought before the appropriate courts, departments, offices or agencies of the government situated in Mandaluyong City.<br><br>
+14. The authorized representative must present valid identification papers.<br><br>
+15. The pawner shall advise the pawnshop of any change of address.<br>
+16. Acknowledgment: I hereby declare that the above mentioned article(s) is/are my personal property and are free from liens and encumbrances.<br><br>
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_____________________<br>
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp;PAWNER';
 
-',0,'L',0,1,'','',true);
+// writeHTMLCell($w, $h, $x, $y, $html='', $border=0, $ln=0, $fill=0, $reseth=true, $align='', $autopadding=true)
+
+// get current vertical position
+$y = $pdf->getY();
+
+// set color for background
+$pdf->SetFillColor(255, 255, 200);
+
+// set color for text
+$pdf->SetTextColor(0, 63, 127);
+
+// write the first column
+$pdf->writeHTMLCell(130, '', '', $y, $left_column, 1, 0, 1, true, 'J', true);
+
+// set color for background
+$pdf->SetFillColor(215, 235, 255);
+
+// set color for text
+$pdf->SetTextColor(127, 31, 0);
+
+// write the second column
+$pdf->writeHTMLCell(140, '', '', '', $right_column, 1, 1, 1, true, 'J', true);
+// reset pointer to the last page
+$pdf->lastPage();
 
 ob_start();
 // Close and output PDF document
